7906ab72743ca697ddef8581f70c7be2e6ecbdf0
hiroshi.yamazaki <hiroshi.yamazaki@jp.oro.com>
2015-10-26 13:35:36 +0900

    fix user

--- redis-mariadb/mysql-init.sql
+++ redis-mariadb/mysql-init.sql
@@ -1,10 +1,12 @@
 SET NAMES utf8;
 
 CREATE DATABASE mustbuy_dev DEFAULT CHARSET utf8;
+GRANT ALL PRIVILEGES ON mustbuy_dev.* TO oro_dev@localhost IDENTIFIED BY 'EWTnFUmh';
 GRANT ALL PRIVILEGES ON mustbuy_dev.* TO oro_dev@'%' IDENTIFIED BY 'EWTnFUmh';
 
 
 CREATE DATABASE mustbuy_test DEFAULT CHARSET utf8;
+GRANT ALL PRIVILEGES ON mustbuy_test.* TO oro_test@localhost IDENTIFIED BY 'RwozjmRb';
 GRANT ALL PRIVILEGES ON mustbuy_test.* TO oro_test@'%' IDENTIFIED BY 'RwozjmRb';
 
 FLUSH PRIVILEGES;
