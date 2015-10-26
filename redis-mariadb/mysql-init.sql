f6002a571491981d8aea1d925ac61c1557e066d9
hiroshi.yamazaki <hiroshi.yamazaki@jp.oro.com>
2015-10-26 13:24:41 +0900

    add redis and mariadb

---
+++ redis-mariadb/mysql-init.sql
@@ -0,0 +1,10 @@
+SET NAMES utf8;
+
+CREATE DATABASE mustbuy_dev DEFAULT CHARSET utf8;
+GRANT ALL PRIVILEGES ON mustbuy_dev.* TO oro_dev@'%' IDENTIFIED BY 'EWTnFUmh';
+
+
+CREATE DATABASE mustbuy_test DEFAULT CHARSET utf8;
+GRANT ALL PRIVILEGES ON mustbuy_test.* TO oro_test@'%' IDENTIFIED BY 'RwozjmRb';
+
+FLUSH PRIVILEGES;
