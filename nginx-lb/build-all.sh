219537aa6c1c194aacd23510d8fd1139e1f62a63
yamazaki164 <yamazaki164@gmail.com>
2015-11-08 10:24:15 +0000

    rename

--- nginx-lb/build-all.sh
+++ nginx-lb/build-all.sh
@@ -2,6 +2,6 @@
 
 current=`pwd`
 
-cd $current/lv && sh ./build.sh
+cd $current/lb && sh ./build.sh
 cd $current/web1 && sh ./build.sh
 cd $current/web2 && sh ./build.sh
--- nginx-lv/lv/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t nginx-lv:latest .
