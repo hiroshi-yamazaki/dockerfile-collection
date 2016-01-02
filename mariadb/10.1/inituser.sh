9b6a775ff35300d0953e835414a02336dffe0506
hiroshi-yamazaki <hiroshi.yamazaki@jp.oro.com>
2016-01-03 01:23:45 +0900

    rename mariadb

--- mariadb/10.1/inituser.sh
+++ mariadb/10.1/inituser.sh
@@ -4,6 +4,6 @@ DB_USER=root
 DB_PASS=1234qwer
 
 mysqld_safe &
-sleep 3
+sleep 5
 echo "grant all privileges on *.* to ${DB_USER}@'%' identified by '${DB_PASS}' with grant option" | mysql -u root
 
---
+++ mariadb/5.5/inituser.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DB_USER=root
+DB_PASS=1234qwer
+
+mysqld_safe &
+sleep 5
+echo "grant all privileges on *.* to ${DB_USER}@'%' identified by '${DB_PASS}' with grant option" | mysql -u root
+
