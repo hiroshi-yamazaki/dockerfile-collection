510c4d9d4d4d1245903287519479905a2678ebe4
hiroshi-yamazaki <hiroshi.yamazaki@jp.oro.com>
2016-01-03 01:27:13 +0900

    rm yml

--- mariadb/10.1/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -t -i -d -p 3306:3306 -h 'mariadb101' --name 'mariadb101' mariadb:10.1
--- mariadb/5.5/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -t -i -d -p 3306:3306 -h 'mariadb55' --name 'mariadb55' mariadb:5.5
